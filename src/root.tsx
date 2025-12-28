ccd77ce6a696e3975f0a880128297a7b2bd2f9fe
Luke Gareth Ribchester <luke@luke.gr>
2025-12-28 07:35:46 +0100

    feat: create the `example` application
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,48 @@
-export default () => {
+import { component$, isDev } from "@builder.io/qwik";
+import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
+import { RouterHead } from "./example/components/router-head/router-head";
+
+import "./global.css";
+
+export default component$(() => {
+  /**
+   * The root of a QwikCity site always start with the <QwikCityProvider> component,
+   * immediately followed by the document's <head> and <body>.
+   *
+   * Don't remove the `<head>` and `<body>` elements.
+   */
+
   return (
-    <>
+    <QwikCityProvider>
       <head>
-        <meta charset="utf-8" />
-        <title>Qwik Blank App</title>
+        <meta charset="utf-8"/>
+        {!isDev && (
+          <link
+            rel="manifest"
+            href={`${import.meta.env.BASE_URL}manifest.json`}
+          />
+        )}
+        <RouterHead/>
+
+        <link
+          rel="preload"
+          href="/fonts/poppins/Poppins-Regular.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+
+        <link
+          rel="preload"
+          href="/fonts/pt-mono/PTMono-Regular.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
       </head>
-      <body>
+      <body lang="en">
+      <RouterOutlet/>
       </body>
-    </>
+    </QwikCityProvider>
   );
-};
+});
